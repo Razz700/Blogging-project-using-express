bf46bf9fb8b76c83f2661886ee7d28cfa5216fe7
Razzakh <mrazzakh123@gmail.com>
2024-10-01 13:08:45 +0530

    vercel3

--- router.js
+++ router.js
@@ -3,28 +3,28 @@ const router=express.Router();
 const { signupPage, loginPage, registerUser, login, allUsers, logout } = require('./controllers/userconntroller.js');
 const {requireAuth} = require('./utils/auth.js');
 const {home,addBlog, myBlog, createBlog, deleteBlog, editBlog, updateBlog} = require('./controllers/blogController.js');
-const path='https://blogging-project-using-express.vercel.app/'
+
 ////////////////router.get(signup)//////////////////
-router.get('https://blogging-project-using-express.vercel.app/signup',signupPage);
+router.get('/signup',signupPage);
 ////////////////router.get(login)//////////////////
-router.get('https://blogging-project-using-express.vercel.app/login',loginPage);
+router.get('/login',loginPage);
 //////////////////router.post(register)/////////////
-router.post('https://blogging-project-using-express.vercel.app//register',registerUser);
+router.post('/register',registerUser);
 /////////////////router.post(login)////////////////////
-router.post('https://blogging-project-using-express.vercel.app//login',login);
+router.post('/login',login);
 //////////////////all users/////////////////////
-router.get('https://blogging-project-using-express.vercel.app/allusers',requireAuth,allUsers)
+router.get('/allusers',requireAuth,allUsers)
 ///////////////logout/////////////////
-router.get('https://blogging-project-using-express.vercel.app/logout',logout)
+router.get('/logout',logout)
 /////////////////////home//////////////
-router.get('https://blogging-project-using-express.vercel.app/',home)
+router.get('/',home)
 /////////////////////home//////////////
-router.get('https://blogging-project-using-express.vercel.app/home',home);
-router.get('https://blogging-project-using-express.vercel.app/myblog',requireAuth,myBlog);
-router.get('https://blogging-project-using-express.vercel.app/addblog',requireAuth,addBlog);
-router.get('https://blogging-project-using-express.vercel.app/editblog',requireAuth,editBlog);
-router.post('https://blogging-project-using-express.vercel.app/createblog',requireAuth,createBlog);
-router.post('https://blogging-project-using-express.vercel.app/updateblog',requireAuth,updateBlog);
-router.get('https://blogging-project-using-express.vercel.app/deleteblog',deleteBlog);
+router.get('/home',home);
+router.get('/myblog',requireAuth,myBlog);
+router.get('/addblog',requireAuth,addBlog);
+router.get('/editblog',requireAuth,editBlog);
+router.post('/createblog',requireAuth,createBlog);
+router.post('/updateblog',requireAuth,updateBlog);
+router.get('/deleteblog',deleteBlog);
 
 module.exports={router}
